894232099df5c26dc428c177f22542c2527221dc
seangleng <seangleng507@gmail.com>
2024-01-14 17:58:34 +0700

    done

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,9 +10,9 @@ export default function Footer() {
     return (
         <div className='flex flex-wrap justify-between items-center pb-10'>
             <div className='flex gap-3 justify-start items-center text-4xl text-gray-400'>
-                <a href='' className='hover:scale-110 transition-all'><MdFacebook /></a>
-                <a href='' className='hover:scale-110 transition-all'><FaGithub /></a>
-                <a href='' className='hover:scale-110 transition-all'><PiInstagramLogoFill /></a>
+                <a target='_blank' href='https://www.facebook.com/profile.php?id=100086488516436' className='hover:scale-110 transition-all'><MdFacebook /></a>
+                <a target='_blank' href='https://github.com/SeangLeng' className='hover:scale-110 transition-all'><FaGithub /></a>
+                <a target='_blank' href='https://www.instagram.com/chheng_jenzy/' className='hover:scale-110 transition-all'><PiInstagramLogoFill /></a>
             </div>
             <div>
                 <a href="/" className="font-bold text-secondary-color text-3xl">L<span className="text-white">eng.</span></a>
